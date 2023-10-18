2a661ccb36bc0a4e98ae29f049b0ddde8542921c
Anuja M. Ingle <124343328+anujaingle01@users.noreply.github.com>
2023-10-18 12:35:11 +0000

    Commit

---
+++ 01_basics/Variables.js
@@ -0,0 +1,16 @@
+const accId = 101 // Constant
+let accEmail = "anu@mail.com"
+var accPassword = "123" // Do not use var
+accCity = "Akola" 
+let accName;
+
+// accId = 102
+console.log(accId);
+
+accEmail = "a@mail.com"
+accPassword = "1999"
+accCity = "Pune"    // Same
+accCity = "Mumbai"  // Same 
+
+
+console.table([accId,accEmail,accPassword,accCity,accName])
